284a2a6adb69dd556bca691b11dadc139751716e
Daniel Wood <daniel.j.wood11@gmail.com>
2018-12-20 16:17:04 -0500

    add correct chart titles

--- js/varlist.js
+++ js/varlist.js
@@ -1,123 +1,122 @@
 var varListMaster = {
 	"longChart":{
 		"chartType":"long-bar-chart",
-		"chartTitle":"Multifamily, Commerical, and Industrial Lending Volume per Capita, 2011–16",
-		"textTitle":"Multifamily, Commerical, and Industrial Lending Volume per Capita, 2011–16",
+		"chartTitle":"Multifamily, commercial, and industrial lending volume per capita, 2011–16",
 		"notes":"",
-		"sources":"2011–16 CoreLogic data and 2012–16 American Community Survey data."
+		"sources":"2011–16 CoreLogic data and 2012–16 American Community Survey data.",
+		"id":0
 	},
 	"capFlowRate":{
 		"chartType":"hor-bar-chart",
-		"chartTitle":"Capital Flows per Household by Neighborhood Characteristics, 2011–16",
-		"textTitle":"Capital Flows per Household by Neighborhood Characteristics, 2011–16",
+		"chartTitle":"Capital flows per household by neighborhood characteristics, 2011–16",
 		"notes":"High-poverty neighborhoods have a poverty rate higher than 25 percent, and low-poverty neighborhoods have a poverty rate of 25 percent or lower.",
-		"sources":"2011–16 CoreLogic data, 2012–16 American Community Survey data, 2011–16 Baltimore City Planning Department Capital Improvement Plans, 2011–15 US Department of Housing and Urban Development data, 2011–16 Maryland Department of Education data; 2011–15 Baltimore City Department of Finance data, 2011–16 Community Reinvestment Act Federal Financial Institutions Examination Council reporting, and 2015 Longitudinal Employer-Household Dynamics Origin-Destination Employment Statistics Workplace Area Characteristics data."
+		"sources":"2011–16 CoreLogic data, 2012–16 American Community Survey data, 2011–16 Baltimore City Planning Department Capital Improvement Plans, 2011–15 US Department of Housing and Urban Development data, 2011–16 Maryland Department of Education data; 2011–15 Baltimore City Department of Finance data, 2011–16 Community Reinvestment Act Federal Financial Institutions Examination Council reporting, and 2015 Longitudinal Employer-Household Dynamics Origin-Destination Employment Statistics Workplace Area Characteristics data.",
+		"id":3
 	},
 	"loanChart":{
 		"chartType":"hor-bar-chart",
-		"chartTitle":"Loan amount per owner-occupied housing unit ",
-		"textTitle":"Loan amount per owner-occupied housing unit ",
+		"chartTitle":"Single-family lending volume per owner-occupied housing unit, 2011-16",
 		"notes":"",
-		"sources":"2011–16 CoreLogic data and 2012–16 American Community Survey data."
+		"sources":"2011–16 CoreLogic data and 2012–16 American Community Survey data.",
+		"id":7
 	},
 	"tripleChart1":{
 		"chartType":"hor-bar-chart",
-		"chartTitle":"HUD’s HOME and CDBG Investment",
-		"textTitle":"HUD’s HOME and CDBG Investment",
+		"chartTitle":"Federal HOME and CDBG investment per household, 2011-16",
 		"notes":"CDBG = Community Development Block Grant Program; HOME = HOME Investment Partnerships Program; HUD = US Department of Housing and Urban Development; LIHTC = low-income housing tax credit. Census tracts are high poverty if more than 25 percent of households are below the federal poverty level and are low poverty if less than 25 percent of households are below the federal poverty level.",
-		"sources":"2012–16 American Community Survey data, Baltimore City Planning Department data, and U.S. Department of Housing and Urban Development data."
+		"sources":"2012–16 American Community Survey data, Baltimore City Planning Department data, and U.S. Department of Housing and Urban Development data.",
+		"id":11
 	},	
 	"tripleChart2":{
 		"chartType":"hor-bar-chart",
-		"chartTitle":"Baltimore’s Capital Improvement Program Financing",
-		"textTitle":"Baltimore’s Capital Improvement Program Financing",
+		"chartTitle":"Baltimore Capital Improvement Program financing per household, 2011-16",
 		"notes":"CDBG = Community Development Block Grant Program; HOME = HOME Investment Partnerships Program; HUD = US Department of Housing and Urban Development; LIHTC = low-income housing tax credit. Census tracts are high poverty if more than 25 percent of households are below the federal poverty level and are low poverty if less than 25 percent of households are below the federal poverty level.",
-		"sources":"2012–16 American Community Survey data, Baltimore City Planning Department data, and U.S. Department of Housing and Urban Development data."
+		"sources":"2012–16 American Community Survey data, Baltimore City Planning Department data, and U.S. Department of Housing and Urban Development data.",
+		"id":12
 	},	
 	"investmentShareChart":{
 		"chartType":"stacked-bar-chart",
-		"chartTitle":"Baltimore Investment Share by Type",
-		"textTitle":"Baltimore Investment Share by Type",
+		"chartTitle":"Baltimore investment share by type, 2011-16",
 		"notes":"",
-		"sources":"Baltimore City Planning Department, US Department of Housing and Urban Development, Maryland Department of Education, CoreLogic, CDFI Fund."
+		"sources":"Baltimore City Planning Department, US Department of Housing and Urban Development, Maryland Department of Education, CoreLogic, CDFI Fund.",
+		"id":14
 	},
 	"raceMap":{
 		"chartType":"dot-map",
-		"chartTitle":"Map showing race (and population density) across Baltimore",
-		"textTitle":"Map showing race (and population density) across Baltimore",
-		"notes":"AAPI = Asian American and Pacific Islander.",
-		"sources":"2012–16 American Community Survey data."
+		"chartTitle":"Population distribution of residents by race",
+		"notes":"Each dot represents 1000 residents. AAPI = Asian American and Pacific Islander.",
+		"sources":"2012–16 American Community Survey data.",
+		"id":1
 	},
 	"povrate":{
 		"range":[0.01675042,0.114,0.197,0.269,0.354,1],
 		"chartType":"poly-map",
 		"chartTitle":"Map showing poverty rate by tract",
-		"textTitle":"Map showing poverty rate by tract",
 		"notes":"",
-		"sources":"2012–16 American Community Survey data"
+		"sources":"2012–16 American Community Survey data",
+		"id":2
 	},
 	"permit_hh":{
 		"range":[294.3623962,3766,7227,17287,41543,446386],
 		"chartType":"poly-map",
-		"chartTitle":"This is a test of a title",
-		"textTitle":"This is a test of a title",
+		"chartTitle":"Construction, rehab, and demolition volume per household, 2011-16",
 		"notes":"",
-		"sources":"2011–16 Baltimore housing data via Open Baltimore and 2012–16 American Community Survey data."
+		"sources":"2011–16 Baltimore housing data via Open Baltimore and 2012–16 American Community Survey data.",
+		"id":4
 	},
 	"sales_hh":{
 		"range":[568.6334839,7299,13837,24157,51330,248426],
 		"chartType":"poly-map",
-		"chartTitle":"This is a test of a title",
-		"textTitle":"This is a test of a title",
+		"chartTitle":"Aggregate real estate sales per household, 2011-16",
 		"notes":"Sales are of all land-use types, including multifamily, industrial, commercial, single-family, institutional, parking, and vacant.",
-		"sources":"2011–16 Baltimore housing data via Open Baltimore and 2012–16 American Community Survey data."
+		"sources":"2011–16 Baltimore housing data via Open Baltimore and 2012–16 American Community Survey data.",
+		"id":5
 	},	
 	// "agg_hh":{
 	// 	"range":[0,22377,37889,60202,127405,475073],
 	// 	"chartType":"poly-map",
-	// 	"chartTitle":"This is a test of a title",
-	// 	"textTitle":"This is a test of a title",
+	// 	"chartTitle":"This is a test of a title"
 	// 	"notes":"",
 	// 	"sources":""
 	// },
 	"res_hh":{
 		"range":[0,12422,24877,39472,74426,277759],
 		"chartType":"poly-map",
-		"chartTitle":"This is a test of a title",
-		"textTitle":"This is a test of a title",
+		"chartTitle":"Aggregate private sector real estate lending volume per household, 2011-16",
 		"notes":"",
-		"sources":"2011–16 CoreLogic data and 2012–16 American Community Survey data."
+		"sources":"2011–16 CoreLogic data and 2012–16 American Community Survey data.",
+		"id":6
 	},
 	"comm_hh":{
 		"range":[0,838,3357,8912,24804,419182],
 		"chartType":"poly-map",
-		"chartTitle":"This is a test of a title",
-		"textTitle":"This is a test of a title",
+		"chartTitle":"Commercial real estate lending volume per household, 2011-16",
 		"notes":"",
-		"sources":"2011–16 CoreLogic data and 2012–16 American Community Survey data."
+		"sources":"2011–16 CoreLogic data and 2012–16 American Community Survey data.",
+		"id":8
 	},
 	"CRA_hh":{
 		"range":[7.587253571,81,199,474,1287,10670],
 		"chartType":"poly-map",
-		"chartTitle":"This is a test of a title",
-		"textTitle":"This is a test of a title",
+		"chartTitle":"CRA small business lending volume per household, 2011-16",
 		"notes":"",
-		"sources":"2004–16 Community Reinvestment Act Federal Financial Institutions Examination Council reporting and 2015 Longitudinal Employer-Household Dynamics Origin-Destination Employment Statistics Workplace Area Characteristics data."
+		"sources":"2004–16 Community Reinvestment Act Federal Financial Institutions Examination Council reporting and 2015 Longitudinal Employer-Household Dynamics Origin-Destination Employment Statistics Workplace Area Characteristics data.",
+		"id":9
 	},
 	"pub_hh":{
 		"range":[0,12,425,1164,3111,150313],
 		"chartType":"poly-map",
-		"chartTitle":"This is a test of a title",
-		"textTitle":"This is a test of a title",
+		"chartTitle":"Public sector investment per household, 2011-16",
 		"notes":"",
-		"sources":"2012–16 American Community Survey data, 2011–16 Baltimore City Planning Department Capital Improvement Plans, 2011–15 US Department of Housing and Urban Development data, 2011–16 Maryland Department of Education data, and 2011–15 Baltimore City Department of Finance data."
+		"sources":"2012–16 American Community Survey data, 2011–16 Baltimore City Planning Department Capital Improvement Plans, 2011–15 US Department of Housing and Urban Development data, 2011–16 Maryland Department of Education data, and 2011–15 Baltimore City Department of Finance data.",
+		"id":10
 	},
 	"miss_hh":{
 		"range":[0,1079,1800,3151,4324,115561],
 		"chartType":"poly-map",
-		"chartTitle":"This is a test of a title",
-		"textTitle":"This is a test of a title",
+		"chartTitle":"Aggregate mission lending volume per household, 2011-2016",
 		"notes":"",
-		"sources":"2011–16 CoreLogic data, 2012–16 American Community Survey data, US Department of the Treasury’s Community Development Financial Institutions Fund Transaction Level Report database, and Opportunity Finance Network."
+		"sources":"2011–16 CoreLogic data, 2012–16 American Community Survey data, US Department of the Treasury’s Community Development Financial Institutions Fund Transaction Level Report database, and Opportunity Finance Network.",
+		"id":13
 	}
 }; 
